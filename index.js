d1189d49b8d73d23d10bbc5cafa6e31bfa3fb61a
Jose Cabrera <josemcabrerat@gmail.com>
2024-12-13 06:47:16 -0700

    Added code where the user is asked for the user input questions.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
 
+import inquirer from "inquirer"; 
+import fs from 'fs';
+//import { writeFile } from 'fs/promises';
+
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Whats your project Title?:",
+    "Enter Description:",
+    "How do you Install the application?:",
+    "Usage:",
+    "Please enter the Contribuitors:",
+    "Tests Instructions:"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
