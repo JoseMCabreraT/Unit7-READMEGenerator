fd1644c9388c7db5f7fae63d4f70a181af27b1b2
Jose Cabrera <josemcabrerat@gmail.com>
2024-12-14 07:09:16 -0700

    The inquirer prompt and questions for the user are almost done. Also, I have the first generated README file.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Unit7-READMEGenerator
+
+        # Challenge 7 README Generator
+
+        ## Description
+        test 1
+
+        ## Installation
+        test 2
+
+        ## Usage
+        test 3
+
+        ## Contributing
+        Jose Cabrera
+
+        ## Test
+        test 4
+
+        ## License
+        MIT
+
+        ## Credits
+        josemcabrerat
+        josemcabrerat@gmail.com
+        
--- index.js
+++ index.js
@@ -11,39 +11,111 @@ const questions = [
 
     {
         type: 'input',
+        name: 'title',
+        message: 'Whats your project Title?:',
 
     },
     {
+        type: 'input',
+        name: 'description',
+        message: 'Enter Description:',
 
     },
     {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you Install the application?:',
 
     },
 
     {
 
+    type: 'input',
+    name: 'usage',
+    message: 'Usage:',
     },
 
     {
 
-    },
-    {
+    type: 'input',
+    name: 'Contribuitors',
+    message: 'Please enter the Contribuitors:',
 
     },
-    
-    "Whats your project Title?:",
-    "Enter Description:",
-    "How do you Install the application?:",
-    "Usage:",
-    "Please enter the Contribuitors:",
-    "Tests Instructions:"
-];
+
+    {
+
+    type: 'input',
+    name: 'test',
+    message: 'Tests Instructions:',
+
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license:',
+        choices: ['Apache', 'European Union Public License', 'GNU General Public License v3.0', 'Microsoft', 'MIT', 'Mozilla', 'PostgreSQL'],
+    },
+
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter GitHub username:',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter email address:',
+    },
+]; //array of questions.
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('The README file has been successfully created.');
+    }); 
+} //function for creating the README file.
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+
+      const generateREADME = `
+        # ${answers.title}
+
+        ## Description
+        ${answers.description}
+
+        ## Installation
+        ${answers.installation}
+
+        ## Usage
+        ${answers.usage}
+
+        ## Contributing
+        ${answers.Contribuitors}
+
+        ## Test
+        ${answers.test}
+
+        ## License
+        ${answers.license}
+
+        ## Credits
+        ${answers.username}
+        ${answers.email}
+        `;
+            writeToFile('README.md', generateREADME);
+    });
+} // function for initialize the app.
 
 // Function call to initialize app
 init();
